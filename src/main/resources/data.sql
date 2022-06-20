52270770fa8c9a9e0e808a5baa19673be0a9f7c4
LuizHenriqueLobo1 <luizhenriquewolf@gmail.com>
2022-06-20 17:36:07 -0300

    Alterando data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO CLIENTE(NOME, CNPJ, ENDERECO, DATA)
 VALUES
-('Jack',    '11.111.111/0001-11', 'Rua x, Numero x', '13/06/2022'),
-('Abigail', '22.222.222/0001-22', 'Rua y, Numero y', '01/01/2022'),
-('John',    '33.333.333/0001-33', 'Rua z, Numero z', '12/06/2022');
+('Jack',    '11.111.111/0001-11', 'Rua a, Numero 1', '13/06/2022'),
+('Abigail', '22.222.222/0001-22', 'Rua b, Numero 2', '01/01/2022'),
+('John',    '33.333.333/0001-33', 'Rua c, Numero 3', '12/06/2022');
 
 INSERT INTO CHAMADO(CLIENTE_ID, ASSUNTO, STATUS, COMPLEMENTO, DATA)
 VALUES
